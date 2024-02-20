ffeed2c2a576913421b357cd93b7dca17703ae38
garywongkai <53716255+garywongkai@users.noreply.github.com>
2024-02-20 13:41:40 +0800

    Add Remark Command

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.person.Remark;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RemarkCommandTest {
+
+    @Test
+    void execute() {
+        final Remark remark = new Remark("Some remark");
+    }
+}
