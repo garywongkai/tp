6476cc309aced18f1f442b17061d0efcf18a174f
Morninggloryyy <156987743+Morninggloryyy@users.noreply.github.com>
2024-04-03 16:22:28 +0800

    Update Messages.java

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -18,21 +18,17 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-
     public static final String MESSAGE_INVALID_SCHEDULE_DISPLAYED_INDEX = "The schedule index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_INVALID_DATETIME_FORMAT = "The date format provided is invalid. "
             + "Format: " + getDateTimeStringFormat();
-
     public static final String MESSAGE_OUT_SCOPE_DATETIME = "The schedule time should be between 8:00 to 21:00. "
             + "Format: " + getDateTimeStringFormat();
-
     public static final String MESSAGE_DIFFERENT_DATE = 
             "The schedule start time and end time should be in the same day. "
             + "Format: " + getDateTimeStringFormat();
-
 
     /**
      * Returns an error message indicating the duplicate prefixes.
