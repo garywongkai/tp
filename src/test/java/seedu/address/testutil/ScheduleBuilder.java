e8dccc7c38e365b7b26d48d3034a711625bca553
Morninggloryyy <jinyanwu10@gmail.com>
2024-04-03 13:15:57 +0800

    Fix CheckStyle issue

--- src/test/java/seedu/address/testutil/ScheduleBuilder.java
+++ src/test/java/seedu/address/testutil/ScheduleBuilder.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-import seedu.address.model.person.Person;
 import seedu.address.model.schedule.Schedule;
 
 /**
