079efc6c57b135c713c1528508fe8113f8b83341
Wong Kai Li <53716255+garywongkai@users.noreply.github.com>
2024-04-15 15:40:44 +0800

    Update AddSchedCommandParser.java

--- src/main/java/seedu/address/logic/parser/AddSchedCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddSchedCommandParser.java
@@ -1,16 +1,12 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.Messages.MESSAGE_DIFFERENT_DATE;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_DATETIME_FORMAT;
-import static seedu.address.logic.Messages.MESSAGE_OUT_SCOPE_DATETIME;
-import static seedu.address.logic.Messages.MESSAGE_START_LATE_THAN_END;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SCHEDULE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
-
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
